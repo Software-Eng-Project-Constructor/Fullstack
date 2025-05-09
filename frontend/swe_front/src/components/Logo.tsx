8b402039ac62feb2415e11a386a27fa93b6b059c
basma <bsalim@constructor.university>
2025-05-09 12:30:06 +0200

    logo update

--- frontend/swe_front/src/components/Logo.tsx
+++ frontend/swe_front/src/components/Logo.tsx
@@ -1,5 +1,12 @@
-const Logo = () => (
-    <img src="/assets/logo.jpg" alt="Logo" className="h-15 w-auto" />
-  );
+// const Logo = () => (
+//     <img src="/assets/logo.png" alt="Logo" className="h-16 w-auto" />
+//   );
   
+// export default Logo;
+const Logo = () => (
+  <div className="flex items-center space-x-2">
+    <img src="/assets/logo.png" alt="Logo" className="h-20 w-auto" />
+    <span className="text-orange-400 text-2xl font-mono">Project Manager</span>
+  </div>
+);
 export default Logo;
